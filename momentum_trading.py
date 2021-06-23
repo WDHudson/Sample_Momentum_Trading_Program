c21338a8ab250e4f3da12c8c0abd295926c94df0
William Hudson <williamdhudson@gmail.com>
2021-06-23 17:10:30 -0400

    Added helpers file

--- momentum_trading.py
+++ momentum_trading.py
@@ -5,6 +5,7 @@ import math
 from scipy import stats
 import xlsxwriter
 from secrets import IEX_CLOUD_API_TOKEN
+from helpers import chunks
 
 
 stocks = pd.read_csv('sp_500_stocks.csv')
@@ -12,6 +13,4 @@ stocks = pd.read_csv('sp_500_stocks.csv')
 symbol = 'AAPL'
 api_url = f'https://sandbox.iexapis.com/stable/stock/{symbol}/stats?token={IEX_CLOUD_API_TOKEN}'
 
-data = requests.get(api_url)
-
-print(data)
+data = requests.get(api_url).json()
