734925c505428c09ab0b9a30755dd279d50821a7
William Hudson <williamdhudson@gmail.com>
2021-06-23 18:28:45 -0400

    Gets Ticker, Price, 1Yr Return, Number of Shares to Buy

--- momentum_trading.py
+++ momentum_trading.py
@@ -20,7 +20,7 @@ symbol_string = []
 # Goes through each of the items in the list created above...
 for i in range(0, len(symbol_group)):
   # ... and fill into the array just the ticker separated by a comma
-  symbol_string.append(', '.join(symbol_group[i]))
+  symbol_string.append(','.join(symbol_group[i]))
 
 # Make a list of column titles
 my_columns = ['Ticker', 'Price', 'One Year Return', 'Number of Shares to Buy']
@@ -31,4 +31,18 @@ final_dataframe = pd.DataFrame(columns = my_columns)
 for symbol in symbol_string:
   batch_api_url = f'https://sandbox.iexapis.com/stable/stock/market/batch/?types=stats,quote&symbols={symbol}&token={IEX_CLOUD_API_TOKEN}'
   data = requests.get(batch_api_url).json()
+  for ticker in symbol.split(','):
+    final_dataframe = final_dataframe.append(
+      pd.Series(
+        [
+          ticker,
+          data[ticker]['quote']['iexRealtimePrice'],
+          data[ticker]['stats']['year1ChangePercent'],
+          'N/A'
+        ],
+        index = my_columns
+      ),
+      ignore_index = True
+    )
 
+print(final_dataframe)
